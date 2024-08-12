19187ec68f856f29ad7bba35297410f82fb5e514
Bruno <zettca@users.noreply.github.com>
2024-08-12 23:21:59 +0100

    feat: initial configuration

---
+++ index.js
@@ -0,0 +1,20 @@
+export default {
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  overrides: [
+    {
+      files: "pnpm-lock.yaml",
+      options: {
+        rangeEnd: 0,
+      },
+    },
+  ],
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "^react$",
+    "^react-.*",
+    "<THIRD_PARTY_MODULES>",
+    "^~/(.*)$",
+    "^#/(.*)$",
+    "^[./]",
+  ],
+};
